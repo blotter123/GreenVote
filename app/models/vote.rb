106023b26117cb8deddb301c536e6cec6dc493a4
Arjun Raj Jain <jainar@wharton.upenn.edu>
2013-01-05 12:08:37 +0530

    Fixed Voting Bug

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
   attr_accessible :amount, :room_id, :user_id, :building_id
-  validates_presence_of :amount, :room_id, :building_id
+  validates_presence_of :amount, :building_id
   belongs_to :room
   belongs_to :building
 end
