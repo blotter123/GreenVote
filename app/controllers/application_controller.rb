32e9b256f50bc4903bc5954be2b58eae1b7ad79b
Arjun Raj Jain <jainar@wharton.upenn.edu>
2013-01-11 23:01:29 -0500

    Mobile fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,29 @@
 class ApplicationController < ActionController::Base
+  helper :all
   protect_from_forgery
-  #before_filter :authenticate_user!
+  before_filter :prepare_for_mobile
+
+  private
+
   def index
   	@client_ip = request.remote_ip
   	@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
   	@my_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_addr
  	@ip_addr = request.env['REMOTE_ADDR']
   end
+
+
+def mobile_device?
+  if session[:mobile_param]
+    session[:mobile_param] == "1"
+  else
+    request.user_agent =~ /Mobile|webOS/
+  end
+end
+helper_method :mobile_device?
+
+def prepare_for_mobile
+  session[:mobile_param] = params[:mobile] if params[:mobile]
+  request.format = :mobile if mobile_device?
+end
 end
